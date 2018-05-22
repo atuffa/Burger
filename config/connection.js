28aa70cb6a59110bad61f13dd33e7f626c155115
atuffa <tuffajr@gmail.com>
2018-05-22 18:03:55 -0500

    first Deployment

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 let mysql = require("mysql");
+var connection;
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Koji@191991",
-  database: "burger_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "Koji@191991",
+        database: "burger_db",
+    });
+};
 
 module.exports = connection;
